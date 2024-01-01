db071dd5b9ef90c3183e0d2f54dc8dbda90169bd
Bradley Xu <xgheaven@gmail.com>
2024-01-01 22:19:03 +0800

    chore: update deps & add yargs & update test262 (#12)

--- src/cli.ts
+++ src/cli.ts
@@ -3,30 +3,49 @@ import { parseScript } from './parser'
 import * as fs from 'fs'
 import { isExceptionValue, toHostValue } from './value'
 import { initTest262 } from './test262'
+import yargs from 'yargs'
+import { enableLog } from './log'
 // import { enableLog } from "./log";
 
-let file = process.argv[2]
+yargs(process.argv.slice(2))
+  .command(
+    'run <file>',
+    'Run file',
+    (yargs) =>
+      yargs
+        .option('test262', {
+          type: 'boolean',
+          desc: 'Enable test262 mode',
+        })
+        .option('bc-log', {
+          type: 'boolean',
+          desc: 'Enable bytecode running code',
+        })
+        .positional('file', {
+          type: 'string',
+          desc: 'Which file to run',
+        }),
+    (argv) => {
+      const { file, test262: isTest262 } = argv
 
-let isTest262 = false
+      if (argv['bc-log']) {
+        enableLog(true)
+      }
 
-if (file === '--test262') {
-  isTest262 = true
-  file = process.argv[3]
-}
+      const script = fs.readFileSync(file!, 'utf8')
+      const isolute = new Runtime()
+      const context = isolute.newContext()
 
-if (file) {
-  const script = fs.readFileSync(file, 'utf8')
-  const isolute = new Runtime()
-  const context = isolute.newContext()
+      if (isTest262) {
+        initTest262(context)
+      }
 
-  if (isTest262) {
-    initTest262(context)
-  }
-
-  const fn = parseScript(context, script)
-  const ret = context.run(fn)
-  if (isExceptionValue(ret)) {
-    console.log(toHostValue(isolute.currentException!))
-    process.exit(1)
-  }
-}
+      const fn = parseScript(context, script)
+      const ret = context.run(fn)
+      if (isExceptionValue(ret)) {
+        console.log(toHostValue(isolute.currentException!))
+        process.exit(1)
+      }
+    }
+  )
+  .demandCommand().argv
